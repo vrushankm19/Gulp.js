71f89491fb0ee38bea32d5bb82226ff231c3bffa
Vrushank <vrushank.m@amuratech.com>
2023-12-06 13:03:15 +0530

    initial comit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <header></header>
 <main>
     <div class="container">
-        <p>Sany Bharat is a trusted excavator company offering the widest range of construction machinery in India & South Asia. In 2012, Sany Bharat invested more than INR 750 crores to develop infrastructure for R&D, Manufacturing, Quality Inspection, Testing, and Service at their manufacturing facility in Chakan, Pune. Through this facility, the company operates in multiple business verticals viz: Earth Moving, Lifting, Foundation, Mining, Ports, Concrete, Roads and Renewable Energy solutions. Presently, Sany Bharat offers products like excavators, truck-mounted cranes, all-terrain and rough terrain cranes, crawler cranes, transit mixers, batching plants, boom pumps, trailer pumps, piling rigs, motor graders, pavers, milling machines, compactors, reach stackers, rubber tyred gantry cranes, rail-mounted gantry cranes, mining equipment, wind turbine generators and many more.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
     </div>
 </main>
 <footer></footer>
