1115edef72cb2b9d68b67f62f95ca2f2526f0ef0
Vrushank <vrushank.m@amuratech.com>
2023-12-06 13:09:32 +0530

    deleted aall code

--- assets/js/all.js
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-!function(i){"use strict";i(document).ready(function(){AOS.init(),i("[data-popup]").on("click",function(){i(i(this).data("popup")).css("display","flex")}),i(".close").on("click",function(){i(".overlay").hide()})})}.call(window,window.jQuery);
-=======
-<<<<<<< HEAD
-!function(i){"use strict";i(document).ready(function(){AOS.init(),i("[data-popup]").on("click",function(){i(i(this).data("popup")).css("display","flex")}),i(".close").on("click",function(){i(".overlay").hide()})})}.call(window,window.jQuery);
-=======
-!function(n){"use strict";n(document).ready(function(){})}.call(window,window.jQuery);
->>>>>>> 05f6f03 (My Gulp.js V3)
->>>>>>> master
