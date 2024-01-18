830aef8670c48bd150334f25acb4e5b24e14b89f
Vrushank <vrushank.m@amuratech.com>
2024-01-18 13:02:10 +0530

    slider

---
+++ app/js/script.js
@@ -0,0 +1,17 @@
+!function ($) {
+    "use strict";
+    // *********************************************
+    //     document ready start 🚩
+    // ****************************
+    $(document).ready(function () {
+        // *********************************************
+        //     Slider start 🚩
+        // ****************************
+    });
+
+    // Your Code 🗒️
+
+    // *********************************************
+    //     Do not write your code below. ✋😠
+    // ****************************
+}.call(window, window.jQuery); // jquery initializer
